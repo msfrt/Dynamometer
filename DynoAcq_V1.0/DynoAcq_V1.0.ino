49b9fc1cc323ed398d9cf2f4279abf56dcc45162
flipper92017 <59802734+flipper92017@users.noreply.github.com>
2021-12-04 15:16:19 -0500

    Added backbone for ADC reading of strain(mV)

--- DynoAcq_V1.0/DynoAcq_V1.0.ino
+++ DynoAcq_V1.0/DynoAcq_V1.0.ino
@@ -1,9 +1,39 @@
+#include <SPI.h>
+#include <ReadADC.h>
+#include <EasyTimer.h>
+
+#define ADC_CS 20
+
+ADCChip adc(ADC_CS);
+
+ADCSensor strainGauge(8,0,1);
+
+EasyTimer sampleTimer(100);
+EasyTimer sendCAN(10);
+
+#define TORQUE_FACTOR 1
+#define TORQUE_OFFSET 0
+
+int torqueValue = 0;
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(112500);
+  SPI.begin();
+  adc.begin();
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if(sampleTimer.isup()){
+    adc.sample(strainGauge);
+    torqueValue = calculateTorque(strainGauge.avg());
+  }
+
+}
+
+float calculateTorque(int strain){
+  uint16_t torqueNm = 0;
+  torqueNm = (strain * TORQUE_FACTOR) + TORQUE_OFFSET;
+  return torqueNm;
 
 }
